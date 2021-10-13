0310df96068b078595bcbf13416c3803e781d37b
govind6498 <govindsingh6498@gmail.com>
2021-10-13 06:43:44 +0530

    REFECTOR:Ability to create EMployee payroll object on save

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -21,3 +21,44 @@ window.addEventListener("DOMContentLoaded",(event)=>{
         output.textContent = salary.value;
     })
 });
+
+const save=()=>{
+    try{
+        let employeePayrollData = createEmployeePayroll();
+    }
+    catch(e){
+        return;
+    }
+}
+const createEmployeePayroll=()=>{
+    let employeePayrollData = new EmployeePayrollData();
+    try{
+        employeePayrollData.name = getInputValueById("#name");
+    }
+    catch(e){
+        setTextValue(".text-error",e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name=department]").pop();
+    employeePayrollData.salary = getSelectedValues("#salary");
+    employeePayrollData.note = getSelectedValues("#notes");
+    let date = getInputValueById("#day")+" "+getInputValueById("#month")+" "+
+                getInputValueById("#year");
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+const getSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item=>{
+        if(item.chacked) selItems.push(item.value);
+    });
+    return selItems;
+}
+const getInputValueById = (id)=>{
+    let value = document.querySelector(id).value
+    return value;
+}
