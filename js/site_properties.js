d69b3d5cee42466fb552ffe13450394c4f03bd23
govind6498 <govindsingh6498@gmail.com>
2021-10-17 04:04:16 +0530

    REFECTOR:Including site_properties

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,4 @@
 let site_properties = {
-    home_pag:"../pages/test_home.html",
-    add_emp_payroll_page:"..pages/new_payroll_form_js.html"
+    home_page:"../pages/new_home.html",
+    add_emp_payroll_page:"../pages/employeePayroll.html"
 }
