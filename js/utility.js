eeead92231ea44c2378d569c8e7aa8b516a711f4
govind6498 <govindsingh6498@gmail.com>
2021-10-17 03:18:51 +0530

    ADDED:to Add update method

--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,10 @@ let stringfyDate=(date)=>{
     const newDate = !date?"undefined":new date(Date.parse(date)).toLocaleDateString("en-GB",options);
     return newDate
 }
+
+const update = (node)=>{
+    let employeePayrollData = empPayrollList.find(empData=>empData._id==node._id)
+    if(!employeePayrollData) return;
+    localStorage.setItem("editEmp",JSON.stringify(employeePayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
