03a8215c2bbac249e5f9ea31aa744b7bb5207537
govind6498 <govindsingh6498@gmail.com>
2021-10-19 01:11:06 +0530

    MODIFIED:use local storage field

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
-let site_properties = {
-    home_page:"../pages/new_home.html",
-    add_emp_payroll_page:"../pages/employeePayroll.html"
-}
+const site_properties = {
+    use_local_storage: "true",
+    home_page: "../pages/new_home.html",
+    add_employee_payroll_page: "../pages/employeePayroll.html",
+    server_url: "http://127.0.0.1:3000/EmployeePayrollDB"
+} 
