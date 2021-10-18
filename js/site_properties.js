c4d9c4461ce2d1efe74bcc438337e0c9e0eb34ef
govind6498 <govindsingh6498@gmail.com>
2021-10-19 01:53:27 +0530

    MODIFIED:set load from local storage to false

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 const site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/new_home.html",
     add_employee_payroll_page: "../pages/employeePayroll.html",
     server_url: "http://127.0.0.1:3000/EmployeePayrollDB"
