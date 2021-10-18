c10cfd3b1ab3d76f624fc574f78d00ae55fcc9d9
govind6498 <govindsingh6498@gmail.com>
2021-10-19 02:48:56 +0530

    REFECTOR:Function to retrieve data from json server

--- js/new_home.js
+++ js/new_home.js
@@ -46,51 +46,15 @@ const remove = (node) => {
     const index = empPayrollList
         .map(empData => empData.id)
         .indexOf(empPayrollData.id);
-    empPayrollList.splice(index, 1);
+    empPayrollList.splice(index, 1,employeePayrollObj);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 }
-const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            _name: 'Narayan Mahadevan',
-            _gender: "male",
-            _department: [
-                "Engineering",
-                "Finance"
-            ],
-            _salary: "520000",
-            _startDate: "15 Oct 2021",
-            _note: "Hello Narayan",
-            id: new Date().getTime(),
-            _profilePic: "../assets/profile3.webp"
-        },
-        {
-            _name: 'Govind Singh',
-            _gender: "male",
-            _department: [
-                "Manager"
-            ],
-            _salary: "500000",
-            _startDate: "12 Nov 2021",
-            _note: "",
-            id: new Date().getTime() + 1,
-            _profilePic: "../assets/profile2.webp"
-        },
-        {
-            _name: 'Anoo Sing',
-            _gender: "Female",
-            _department: [
-                "HR",
-                "finance"
-            ],
-            _salary: "426000",
-            _startDate: "10 December 2021",
-            _note: "",
-            id: new Date().getTime() + 1,
-            _profilePic: "../assets/profile1.webp"
-        }
-    ];
-    return empPayrollListLocal;
+const update = (node)=>{
+    let employeePayrollData = empPayrollList.find(empData=>empData._id==node._id)
+    if(!employeePayrollData) return;
+    localStorage.setItem("editEmp",JSON.stringify(employeePayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
 }
+
