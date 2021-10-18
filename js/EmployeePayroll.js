8a6b6913bb9c0f3a75880e67ecdaa07aa3bc8db0
govind6498 <govindsingh6498@gmail.com>
2021-10-19 00:25:05 +0530

    MODIFIED:Getter and Setter for id removed

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,11 +1,5 @@
 class EmployeePayrollData{
-        //getter and setter method
-        get id(){
-            return this._id;
-        }
-        set id(id){
-            this._id = id;
-        }
+        id;
         get name(){
             return this._name;
         }
